26691531b5c21e7a6474d24951528e0ac6815fa2
Rutik2007 <rutiks928@gmail.com>
2026-01-24 11:22:35 +0530

    Add files via upload

---
+++ public1.java
@@ -0,0 +1,14 @@
+class mathutil
+{
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+}
+	public class public1
+	{
+		public static void main(String[] args)
+		{
+			System.out.println(mathutil.add(3,5));
+		}
+	}
