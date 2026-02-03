cb39922dc63a3c1cae302e41024fecddd12847e8
Rutik2007 <rutiks928@gmail.com>
2026-02-03 09:18:05 +0530

    Add files via upload

--- basic.java
+++ basic.java
@@ -1,3 +1,21 @@
 class basic 
 {
-	
+		String name;
+		int id;
+		basic(String name,int id)
+		{
+			this.name=name;
+			this.id=id;
+		}
+
+void display()
+{
+	System.out.println("name" +name+ "id"+id);
+}
+	public static void main(String[] args)
+	{
+		basic b=new basic("rutik",102);
+		b.display();
+	}
+}
+
