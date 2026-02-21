1d1f89ed1a6a26a03bbae45d68c11fea67f19afb
Rutik2007 <rutiks928@gmail.com>
2026-02-21 18:05:22 +0530

    Add files via upload

---
+++ OuterInner.java
@@ -0,0 +1,22 @@
+class Outer
+ {
+    int x=10;
+
+    class Inner 
+	{
+        void display() 
+		{
+            System.out.println("Value of x="+x);
+        }
+    }
+}
+
+public class OuterInner 
+{
+    public static void main(String[] args)
+	{
+        Outer obj = new Outer();
+        Outer.Inner in = obj.new Inner();
+        in.display();
+    }
+}
