45062d5e743817e667d68b7d148926c1ba74e6d1
Rutik2007 <rutiks928@gmail.com>
2026-02-22 08:23:22 +0530

    Add files via upload

---
+++ Mathconstants.java
@@ -0,0 +1,13 @@
+final class MathConstants
+{
+    final double PI = 3.14;
+}
+
+public class mathconst 
+ {
+    public static void main(String[] args) 
+	{
+        MathConstants m=new MathConstants();
+        System.out.println("PI="+ m.PI);
+    }
+}
